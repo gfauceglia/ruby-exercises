9c7a76f58b7a284e252522f262d11ea8a874f51f
gfauceglia <98237396+gfauceglia@users.noreply.github.com>
2023-06-26 20:11:58 -0300

    More elegant answer to find_beautiful_language

--- ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
@@ -41,12 +41,7 @@ end
 def find_beautiful_languages(languages)
   # Take languages and return a hash containing only languages which have the
   # key/value pair { is_beautiful?: true } listed in their information
-  languages.each do |language, value|
-    unless value[:is_beautiful?]
-      languages.delete(language)
-    end
-  end
-  languages
+  languages.select { |language, value| value[:is_beautiful?] }
 end
 
 def find_language_facts(languages, language_name, fact_index = 0)
